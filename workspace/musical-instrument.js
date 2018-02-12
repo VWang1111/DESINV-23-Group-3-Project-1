469a5ed064bed4faf96e68bf5665b988f8d71f34
vwang1111 <vwang1111@gmail.com>
2018-02-12 01:02:07 -0500

    Finished musical instrument

--- workspace/musical-instrument.js
+++ workspace/musical-instrument.js
@@ -2,6 +2,10 @@ var randomXA;
 var randomYA;
 var randomXS;
 var randomYS;
+var randomXD;
+var randomYD;
+var randomXF;
+var randomYF;
 var freqA = 110;
 var freqC = 130.81;
 var freqE = 164.81;
@@ -17,6 +21,7 @@ function setup() {
   background(backgroundColor);
   colorMode (HSB);
   textAlign(CENTER);
+  text('Press SPACE to restart', width/2, height-30);
   
   noStroke();
   
@@ -31,11 +36,17 @@ function setup() {
 
 var sizeA = 0;
 var sizeS = 0;
+var sizeD = 0;
+var sizeF = 0;
 var keyPress = 0;
 var keyA = false;
 var keyS = false;
+var keyD = false;
+var keyF = false;
 var colorA;
 var colorS;
+var colorD;
+var colorF;
 
 function keyPressed() {
   if (key == 'A'){
@@ -52,6 +63,20 @@ function keyPressed() {
     osc.amp(1);
     colorS = random(90,150);
     fill(colorS,40,100);
+  } else if (key == 'D'){
+    keyD = true;
+    keyPress = 3;
+    osc.freq(freqE);
+    osc.amp(1);
+    colorD = random(180,240);
+    fill(colorD,40,100);
+  } else if (key == 'F'){
+    keyF = true;
+    keyPress = 4;
+    osc.freq(freqG);
+    osc.amp(1);
+    colorF = random(270,330);
+    fill(colorF,40,100);
   } else if (keyCode == 32){
     background(backgroundColor);
     sizeA = 0;
@@ -62,6 +87,10 @@ function keyPressed() {
 	randomYA = random(height);
   randomXS = random(width);
 	randomYS = random(height);
+  randomXD = random(width);
+	randomYD = random(height);
+  randomXF = random(width);
+	randomYF = random(height);
 }
 
 function keyReleased() {
@@ -72,6 +101,14 @@ function keyReleased() {
   } else if (key == 'S'){
     keyPress = 0;
     sizeS = 0;
+    osc.amp(0);
+  } else if (key == 'D'){
+    keyPress = 0;
+    sizeD = 0;
+    osc.amp(0);
+  } else if (key == 'F'){
+    keyPress = 0;
+    sizeF = 0;
     osc.amp(0);
   }
 }
@@ -87,6 +124,14 @@ function draw() {
     fill (colorS, 40, 100);
     ellipse (randomXS, randomYS, sizeS, sizeS);
     sizeS += 2;
+  } else if (keyPress == 3) {
+    fill (colorD, 40, 100);
+    ellipse (randomXD, randomYD, sizeD, sizeD);
+    sizeD += 2;
+  } else if (keyPress == 4) {
+    fill (colorF, 40, 100);
+    ellipse (randomXF, randomYF, sizeF, sizeF);
+    sizeF += 2;
   }
   
 
