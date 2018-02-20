80e3cee4600b7bb1017b3f6e3cd0722b46eda512
vwang1111 <vwang1111@gmail.com>
2018-02-20 15:21:38 -0500

    finished visuals

--- workspace/musical-instrument.js
+++ workspace/musical-instrument.js
@@ -1,39 +1,98 @@
-var notes = [48, 50, 53, 55, 60];
+var notes = [];
 var timestep = 0;
 
 function setup () {
 	createCanvas(400, 400);
   colorMode(HSB)
 	frameRate(2);
-  background(255);
+  background(0);
 
 		
 }
 
 function draw () {
-	stroke(0);
-	var currentNote = notes[timestep]; //current note is between 48 and 60
-  var rawInterval = currentNote - 47; //interval is between 1 and 13
-  var interval = rawInterval * 10; // interval multiplier to space out lines on the canvas
-	for (i = 0; i < height; i += interval){
-		line(mouseX, mouseY, 0, i);
-	}
-	
-	for (i = 0; i < width; i += interval){
-		line(mouseX, mouseY, i, 0);
-	}
-	
-	for (i = 0; i < height; i += interval){
-		line(mouseX, mouseY, width, i);
-	}
-	
-	for (i = 0; i < width; i += interval){
-		line(mouseX, mouseY, i, height)
-	}
-  
-  timestep++;
-  if (timestep > notes.length - 1){
-    timestep = 0;
-    background(255);
+  if(notes.length > 15) {
+    var currentNote = notes[timestep]; //current note is between 48 and 60
+    var rawInterval = currentNote - 47; //interval is between 1 and 13
+    var interval = rawInterval * 10; // interval multiplier to space out lines on the canvas is between 10 and 130
+    var color = (360/13) * rawInterval; //split color wheel into 13
+    for (i = 0; i < height; i += interval){
+      stroke(color, 100, 100);
+      line(mouseX, mouseY, 0, i);
+    }
+
+    for (i = 0; i < width; i += interval){
+      stroke(color, 100, 100);
+      line(mouseX, mouseY, i, 0);
+    }
+
+    for (i = 0; i < height; i += interval){
+      stroke(color, 100, 100);
+      line(mouseX, mouseY, width, i);
+    }
+
+    for (i = 0; i < width; i += interval){
+      stroke(color, 100, 100);
+      line(mouseX, mouseY, i, height)
+    }
+
+    timestep++;
+    if (timestep > notes.length - 1){
+      timestep = 0;
+      background(0);
+    }
   }
+}
+
+function keyPressed(){
+	if(notes.length > 15){
+		return false;
+	}
+	else if (keyCode == 65)	{
+		append(notes, 48);
+	}
+	else if (keyCode == 87){
+		append(notes, 49);	
+	}
+	else if (keyCode == 83){
+		append(notes, 50);		
+	}
+	else if (keyCode == 69){
+		append(notes, 51);	
+	}
+	else if (keyCode == 68){
+		append(notes, 52);		
+	}
+	else if (keyCode == 70){
+		append(notes, 53);	
+	}
+	else if (keyCode == 84){
+		append(notes, 54);	
+	}
+	else if (keyCode == 71){
+		append(notes, 55);	
+	}
+	else if (keyCode == 89){
+		append(notes, 56);
+	}
+	else if (keyCode == 72){
+		append(notes, 57);	
+	}
+	else if (keyCode == 85){
+		append(notes, 58);	
+	}
+	else if (keyCode == 74){
+		append(notes, 59);	
+	}
+	else if (keyCode ==75){
+		append(notes, 60);	
+	}
+	else if (keyCode == 192){
+		append(notes, 0);	
+	}
+	else{
+		print("NOT A NOTE");
+		return false;
+	}
+	print(notes.length);
 }
